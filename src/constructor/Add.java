4dab3225547781d89561f9737cc0dce05384d0c1
Dinesh <dineshshane2@gmail.com>
2022-07-11 20:45:54 +0530

    insideConstructor

---
+++ src/constructor/Add.java
@@ -0,0 +1,43 @@
+package constructor;
+
+public class Add {
+	
+		
+
+		
+		
+		Add (int a){
+		{System.out.println("hello");
+		System.out.println("10L per annum");
+		}}
+		
+		
+		Add(int i , int w){
+	       
+		{System.out.println("Dinesh");
+		System.out.println("manju");
+		}
+	}
+
+	      Add  (int j , String str){
+		{System.out.println("Be");
+		System.out.println("IT FIELD");
+		
+		}}
+		
+		public static void end () {
+			{	System.out.println("main ends");
+			}}
+
+		
+		public static void main(String[] args) {
+			Add ab = new Add(30);
+			Add abc =new Add(20,"dinesh");
+			 Add aaa = new Add (30,30);
+	     
+		
+		}
+	
+
+
+}
